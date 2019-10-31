41dfb258afaf0d1362437376367e59ccf9cc7bf8
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-31 15:29:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,18 +2,25 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  hash.collect.each_with_index do |name, value, index|
+  hash.collect.each_with_index do |a, b|
+    if a > b
+      hash.rotate! -1
 
-
- "#{name[0]}"
+ #{}"#{name[0]}"
 end
 end
 
 # iterate through the hash array, collecting key(name) value(value) and index
-#return "name" as my key value
+
+#return "name" as my key value in the first index spot
 
 
 #  value.each do |a, b|
 #    a <=> b
 #    return name[0]
 #end
+
+
+#The way I have mine set up is with 2 variables to use to compare everything
+ #in the loop to. If your value is lower than that variable, 
+ #reassign the variable to that new lower value
