f9cdcad7d6576c6f11d68345ad5437abd90d2d5e
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-31 17:30:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,9 @@ def key_for_min_value(hash)
   default_key = 0
 
   hash.collect do |name, value|
-    if hash.length == 0
+    if hash.empty?
       return nil
-    
+
     elsif value < default
       default = value
       default_key = name
