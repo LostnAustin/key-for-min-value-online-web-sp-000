12dac8e5965057ccc6153183fc7fb3626607b03f
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-31 15:39:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
+  array = []
   hash.collect.each_with_index do |a, b|
-    if a > b
-      hash.rotate! -1
+  
+    
 
  #{}"#{name[0]}"
 end
